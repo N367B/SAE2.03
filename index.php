cdabdf6f3d793de50cbfbaa4f6aa482bd4ed28f7
Noe <bodinoe85@gmail.com>
2022-06-01 14:57:19 +0200

    fs

--- index.php
+++ index.php
@@ -33,46 +33,68 @@
 <?php
 
 /*----------------------------*/
-
-if ($_POST['submit'] == 'Connexion') {
-  include 'requetes.php';
-  $name = $_POST['username'];
-  $pdo = new PDO('sqlite:bdd.sqlite');
-  $query = $requetes[0];
-  $stmt = $pdo->prepare($query);
-  $stmt->bindValue(1, $_POST['username'], PDO::PARAM_STR);
-  $stmt->bindValue(2, md5($_POST['password']), PDO::PARAM_STR);
-  $stmt->execute();
-  $result = $stmt->fetchAll();
-  $pdo = null;
-  if (isset($result[0][1])) {
-    session_start();
-    $_SESSION["newsession"] = $result[0]['id_membre'];
-    $_SESSION['username'] = $_POST['username'];
-    header('Location: place.php');
-  } else {
-    echo "<h3>Identifiant ou mot de passe incorrecte<h3>";
-  }
-}
-
-if ($_POST['submit'] == 'Inscription') {
-  $name = $_POST['username'];
-  if ($name !== '') {
-    #if ()
+if (isset($_POST['submit'])) {
+  if ($_POST['submit'] == 'Connexion') {
     include 'requetes.php';
+    $name = $_POST['username'];
     $pdo = new PDO('sqlite:bdd.sqlite');
-    $query = $requetes[1];
+    $query = $requetes[0];
     $stmt = $pdo->prepare($query);
     $stmt->bindValue(1, $_POST['username'], PDO::PARAM_STR);
     $stmt->bindValue(2, md5($_POST['password']), PDO::PARAM_STR);
-    $stmt->bindValue(3, date('Y-m-d H:i:s'), PDO::PARAM_STR);
     $stmt->execute();
+    $result = $stmt->fetchAll();
     $pdo = null;
-    echo '<h3>Utilisateur ' . $_POST['username'] . ' créé</h3>';
+    if (isset($result[0][1])) {
+      session_start();
+      $_SESSION["newsession"] = $result[0]['id_membre'];
+      $_SESSION['username'] = $_POST['username'];
+      header('Location: place.php');
+    } else {
+      echo "<h3>Identifiant ou mot de passe incorrecte<h3>";
+    }
+  }
+
+  if ($_POST['submit'] == 'Inscription') {
+    $name = $_POST['username'];
+    if (checkName($name)) {
+      #if ()
+      include 'requetes.php';
+      $pdo = new PDO('sqlite:bdd.sqlite');
+      $query = $requetes[1];
+      $stmt = $pdo->prepare($query);
+      $stmt->bindValue(1, $_POST['username'], PDO::PARAM_STR);
+      $stmt->bindValue(2, md5($_POST['password']), PDO::PARAM_STR);
+      $stmt->bindValue(3, date('Y-m-d H:i:s'), PDO::PARAM_STR);
+      $stmt->execute();
+      $pdo = null;
+      echo '<h3>Utilisateur ' . $_POST['username'] . ' créé</h3>';
+    } else {
+      echo "<h3>Mauvais identifiant</h3>";
+    }
+  }
+} else {}
+
+function checkName($name){
+  if ($name == str_replace(' ', '', $name)){
+    include 'requetes.php';
+    $pdo = new PDO('sqlite:bdd.sqlite');
+    $query = $requetes[2];
+    $stmt = $pdo->prepare($query);
+    $stmt->execute();
+    $resultA = $stmt->fetchAll();
+    $pdo = null;
+    foreach ($resultA as $A) {
+      if ($A[1] == $name) {
+        return False;
+      }
+    }
+    return True;
   } else {
-    echo "<h3>Mauvais identifiant</h3>";
+    return False;
   }
 }
+
 
 
 ?>
--- requetes.php
+++ requetes.php
@@ -2,8 +2,10 @@
 <html>
 <?php
 $requetes = [
-  "SELECT * FROM Utilisateur WHERE nom_utilisateur=? AND mot_de_passe=?",
-  "INSERT INTO Utilisateur(nom_utilisateur, mot_de_passe, heure_dernier_pixel) VALUES (?,?,?)"
+  "SELECT * FROM Utilisateur WHERE nom_utilisateur=? AND mot_de_passe=?;",
+  "INSERT INTO Utilisateur(nom_utilisateur, mot_de_passe, heure_dernier_pixel) VALUES (?,?,?);",
+  "SELECT * FROM Utilisateur;",
+  "SELECT * FROM Pixel;"
 ];
 
 ?>
