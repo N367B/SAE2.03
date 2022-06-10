20aaec33eaf1034e55fe8f7a9abb87523f29ea2c
91NL <106147527+91NL@users.noreply.github.com>
2022-06-10 11:41:30 +0200

    Update requetes.php

--- requetes.php
+++ requetes.php
@@ -6,12 +6,13 @@
 */
 $requetes = [
   "SELECT * FROM Utilisateur WHERE nom_utilisateur=? AND mot_de_passe=?;", // Recherche d'un utilisateur en fonction de son nom et de son mot de passe.
-  "INSERT INTO Utilisateur(nom_utilisateur, mot_de_passe, heure_dernier_pixel) VALUES (?,?,?);", // Inscription d'un utilisateur.
+  "INSERT INTO Utilisateur(nom_utilisateur, mot_de_passe, heure_dernier_pixel, nb_pixels_poses) VALUES (?,?,?,?);", // Inscription d'un utilisateur.
   "SELECT * FROM Utilisateur;", // Recuperation de la liste des utilisateurs.
   "SELECT * FROM Pixel;", // Recuperation de la liste des pixels.
   "UPDATE Pixel SET couleur = ?, identifiant_utilisateur = ? WHERE coordonne_x = ? AND coordonne_y = ?;", // Modification d'un pixel.
-  "UPDATE Utilisateur SET heure_dernier_pixel = ? WHERE identifiant_utilisateur = ?", // Modification de l'heure de la derniere modification d'un pixel.
-  "SELECT heure_dernier_pixel FROM Utilisateur WHERE identifiant_utilisateur = ?" // Recuperation de l'heure de la derniere modification d'un pixel.
+  "UPDATE Utilisateur SET heure_dernier_pixel = ?, nb_pixels_poses=(SELECT nb_pixels_poses FROM Utilisateur WHERE identifiant_utilisateur = ?)+1 WHERE identifiant_utilisateur = ?", // Modification de l'heure de la derniere modification d'un pixel.
+  "SELECT heure_dernier_pixel FROM Utilisateur WHERE identifiant_utilisateur = ?", // Recuperation de l'heure de la derniere modification d'un pixel.
+  "SELECT nom_utilisateur, nb_pixels_poses FROM Utilisateur ORDER BY nb_pixels_poses DESC;"
 ];
 
 ?>
