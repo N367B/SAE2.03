0877d0ec3ca1a92caa4f33c27d24313c8db71ace
91NL <106147527+91NL@users.noreply.github.com>
2022-06-10 11:40:50 +0200

    Update init.php

--- init.php
+++ init.php
@@ -20,12 +20,13 @@ if ($_GET["validation"] == "oui") {
   unlink('bdd.sqlite');
   $database = new PDO('sqlite:bdd.sqlite');
   $database->exec("CREATE TABLE `Pixel` ( `coordonne_x` INTEGER, `coordonne_y` INTEGER, `identifiant_utilisateur` TEXT NOT NULL, `couleur` TEXT, FOREIGN KEY(`identifiant_utilisateur`) REFERENCES `Utilisateur`(`identifiant_utilisateur`), PRIMARY KEY(`coordonne_x`,`coordonne_y`) );");
-  $database->exec("CREATE TABLE `Utilisateur` ( `identifiant_utilisateur` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `nom_utilisateur` TEXT NOT NULL UNIQUE, `mot_de_passe` TEXT NOT NULL, `heure_dernier_pixel` TEXT NOT NULL );");
-  $rqt="INSERT INTO Utilisateur (nom_utilisateur, mot_de_passe, heure_dernier_pixel) VALUES (?,?,?);";
+  $database->exec("CREATE TABLE `Utilisateur` ( `identifiant_utilisateur` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `nom_utilisateur` TEXT NOT NULL UNIQUE, `mot_de_passe` TEXT NOT NULL, `heure_dernier_pixel` TEXT NOT NULL, `nb_pixels_poses`  INTEGER);");
+  $rqt="INSERT INTO Utilisateur (nom_utilisateur, mot_de_passe, heure_dernier_pixel, nb_pixels_poses) VALUES (?,?,?,?);";
   $stmt=$database->prepare($rqt);
   $stmt->bindValue(1,'admin',PDO::PARAM_STR);
   $stmt->bindValue(2,md5('admin'),PDO::PARAM_STR);
   $stmt->bindValue(3,microtime(true),PDO::PARAM_STR);
+  $stmt->bindValue(4,0,PDO::PARAM_INT);
   $stmt->execute();
   $rqt="INSERT INTO Pixel (coordonne_x, coordonne_y, couleur, identifiant_utilisateur) VALUES (?,?,?,?);";
   for ($i=0; $i < 16 ; $i++) {
